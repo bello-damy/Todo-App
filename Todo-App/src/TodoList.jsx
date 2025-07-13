0db525fc8dde67cbb7df8d6e5491f2a3a7028d88
Yussuf Bello <yussufbello@Yussufs-MBP.lan>
2025-07-13 19:29:06 +0100

    app functionality complete

--- Todo-App/src/TodoList.jsx
+++ Todo-App/src/TodoList.jsx
@@ -25,10 +25,22 @@ function TodoList() {
 
   function moveTaskUp(index) {
 
+    if (index > 0) {
+      const updatedTasks = [...tasks];
+      [updatedTasks[index], updatedTasks[index - 1]] =
+        [updatedTasks[index - 1], updatedTasks[index]];
+      setTasks(updatedTasks);
+    };
   };
 
   function moveTaskDown(index) {
 
+    if (index < tasks.length - 1) {
+      const updatedTasks = [...tasks];
+      [updatedTasks[index], updatedTasks[index + 1]] =
+        [updatedTasks[index + 1], updatedTasks[index]];
+      setTasks(updatedTasks);
+    };
   };
 
   return (
